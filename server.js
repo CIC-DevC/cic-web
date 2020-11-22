f19e4bfae7cc005a4ee6c3c8ad534e5c6778d051
DESKTOP-DBNOJG4\Quan Bui <quanbv98@gmail.com>
2020-11-22 17:11:27 +0700

    update

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+console.log('server starting');
 var express = require('express');
 var history = require('connect-history-api-fallback');
 app = express();
 app.use(history());
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(__dirname + '/dist'));
 var port = process.env.PORT || 6868;
 app.listen(port);
-console.log("server started at " + port);
-console.log("server started");
+console.log('server started at ' + port);
+console.log('server started');
